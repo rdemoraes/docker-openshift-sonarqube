0e78425b0584c09c0ae6429be5d75877df323003
rdemoraes <raphael.f.moraes@gmail.com>
2018-11-08 20:07:00 -0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export VERSION=6.7.5
-docker build . -t wkulhanek/sonarqube:${VERSION}
-#docker tag wkulhanek/sonarqube:${VERSION} wkulhanek/sonarqube:latest
-docker push wkulhanek/sonarqube:${VERSION}
-#docker push wkulhanek/sonarqube:latest
