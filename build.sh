60ac78e246381e71138fd8debf67070c9e012454
rdemoraes <raphael.f.moraes@gmail.com>
2018-11-19 16:13:45 -0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export VERSION=7.3
-docker build -f Dockerfile7 . -t wkulhanek/sonarqube:${VERSION}
-docker tag wkulhanek/sonarqube:${VERSION} wkulhanek/sonarqube:latest
-docker push wkulhanek/sonarqube:${VERSION}
-docker push wkulhanek/sonarqube:latest
+docker build -f Dockerfile . -t rdemoraes/sonarqube:${VERSION}
+docker tag rdemoraes/sonarqube:${VERSION} wkulhanek/sonarqube:latest
+docker push rdemoraes/sonarqube:${VERSION}
+docker push rdemoraes/sonarqube:latest
